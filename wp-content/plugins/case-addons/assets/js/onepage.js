8aacbd58d0af0af665734c3c784acb604602cb3c
GitHack27 <arceusxy2003@gmail.com>
2025-02-09 23:44:57 +0530

    Reverted

---
+++ wp-content/plugins/case-addons/assets/js/onepage.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:de698f0beeb32a03180886cf1d0938f768624e313b9f9b3f976d1c883c3bf271
+size 2245
---
+++ wp-content/themes/aimo/assets/js/libs/parallax-move-mouse.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6ce46957f611e42857a9eb08e2ae41928b5eb1969e2296710da2d1e28af7c115
+size 1077
